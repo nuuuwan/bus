e3165753676adffe02ecbb11e07a52bd9f09683b
nuuuwan <nuuuwan@gmail.com>
2026-01-31 10:14:31 +0530

    Add RoutePage component for displaying route details and associated bus halts

---
+++ src/view/pages/RoutePage.js
@@ -0,0 +1,63 @@
+import {
+  Box,
+  Typography,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+  CircularProgress,
+} from "@mui/material";
+import { useData } from "../../nonview/contexts/DataContext";
+import MapView from "../moles/MapView";
+
+export default function RoutePage() {
+  const { selectedRoute, loading } = useData();
+
+  if (loading) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (!selectedRoute) {
+    return (
+      <Box p={3}>
+        <Typography variant="h5">Route not found</Typography>
+      </Box>
+    );
+  }
+
+  return (
+    <Box display="flex" height="100vh">
+      <Box width="30%" overflow="auto" p={2}>
+        <Paper elevation={3} sx={{ p: 2, mb: 2 }}>
+          <Typography variant="h5" gutterBottom>
+            {selectedRoute.route_num}
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" gutterBottom>
+            Bus Halts ({selectedRoute.bus_halts.length})
+          </Typography>
+        </Paper>
+        <Paper elevation={3}>
+          <List>
+            {selectedRoute.bus_halts.map((halt, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={`${index + 1}. ${halt}`} />
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      </Box>
+      <Box width="70%">
+        <MapView />
+      </Box>
+    </Box>
+  );
+}
