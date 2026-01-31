d0311005d13f6684d2173eb91380fbefa92e17a0
nuuuwan <nuuuwan@gmail.com>
2026-01-31 10:37:09 +0530

    Wrap main application layout in Box component for improved structure and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 import { DataProvider } from "./nonview/contexts/DataContext";
+import CustomAppBar from "./view/moles/CustomAppBar";
+import CustomBottomNavigator from "./view/moles/CustomBottomNavigator";
 import RootRedirect from "./view/pages/RootRedirect";
 import MapPage from "./view/pages/MapPage";
 import RoutePage from "./view/pages/RoutePage";
@@ -19,12 +22,16 @@ function App() {
       <CssBaseline />
       <DataProvider>
         <BrowserRouter basename="/bus">
-          <Routes>
-            <Route path="/" element={<RootRedirect />} />
-            <Route path="/map/:latLng" element={<MapPage />} />
-            <Route path="/route/:routeNum" element={<RoutePage />} />
-            <Route path="/bus_halt/:name" element={<BusHaltPage />} />
-          </Routes>
+          <Box sx={{ pb: 7 }}>
+            <CustomAppBar />
+            <Routes>
+              <Route path="/" element={<RootRedirect />} />
+              <Route path="/map/:latLng" element={<MapPage />} />
+              <Route path="/route/:routeNum" element={<RoutePage />} />
+              <Route path="/bus_halt/:name" element={<BusHaltPage />} />
+            </Routes>
+            <CustomBottomNavigator />
+          </Box>
         </BrowserRouter>
       </DataProvider>
     </ThemeProvider>
