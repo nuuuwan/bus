ab926a5efe451c6f2ec4949da255392828954dff
nuuuwan <nuuuwan@gmail.com>
2026-01-31 10:32:09 +0530

    Refactor MapView to include MapController for handling map movements and update center based on URL params

--- src/view/moles/MapView.js
+++ src/view/moles/MapView.js
@@ -1,6 +1,9 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { useEffect, useRef } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import LatLng from "../../nonview/base/LatLng";
 
 // Fix for default marker icons in react-leaflet
 delete L.Icon.Default.prototype._getIconUrl;
@@ -10,13 +13,53 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
+function MapController({ center, onMoveEnd }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (center) {
+      map.setView(center, map.getZoom());
+    }
+  }, [center, map]);
+
+  useEffect(() => {
+    map.on("moveend", () => {
+      const mapCenter = map.getCenter();
+      const newLatLng = new LatLng(mapCenter.lat, mapCenter.lng);
+      onMoveEnd(newLatLng);
+    });
+
+    return () => {
+      map.off("moveend");
+    };
+  }, [map, onMoveEnd]);
+
+  return null;
+}
+
 export default function MapView() {
-  const defaultCenter = [6.9271, 79.8612]; // Colombo, Sri Lanka
+  const params = useParams();
+  const navigate = useNavigate();
   const defaultZoom = 13;
+
+  // Parse latLng from URL params
+  const center = params.latLng
+    ? LatLng.fromString(params.latLng).toArray()
+    : LatLng.fromDefault().toArray();
+
+  const handleMoveEnd = (newLatLng) => {
+    const currentLatLng = params.latLng;
+    const newLatLngString = newLatLng.toString();
+
+    // Only update if the latLng has changed
+    if (currentLatLng !== newLatLngString) {
+      navigate(`/map/${newLatLngString}`, { replace: true });
+    }
+  };
 
   return (
     <MapContainer
-      center={defaultCenter}
+      center={center}
       zoom={defaultZoom}
       style={{ height: "100vh", width: "100%" }}
     >
@@ -24,6 +67,7 @@ export default function MapView() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <MapController center={center} onMoveEnd={handleMoveEnd} />
     </MapContainer>
   );
 }
