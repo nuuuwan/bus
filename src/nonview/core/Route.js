9d02f8d0e8b640848f6541c5cef954ffac10108f
nuuuwan <nuuuwan@gmail.com>
2026-02-04 09:26:37 +0530

    Refactor Route class constructor and fromPythonDict method for consistency in parameter naming

--- src/nonview/core/Route.js
+++ src/nonview/core/Route.js
@@ -1,19 +1,19 @@
 import WWW from "../base/WWW";
 import LatLng from "../base/LatLng";
 export default class Route {
-  constructor(routeName, direction, haltNameList, latLngList) {
-    this.routeName = routeName;
+  constructor(routeNum, direction, haltNameList, latLngList) {
+    this.routeNum = routeNum;
     this.direction = direction;
     this.haltNameList = haltNameList;
     this.latLngList = latLngList;
   }
   static fromPythonDict(d) {
     return new Route(
-      d.route_name,
+      d.route_num,
       d.direction,
       d.halt_name_list,
-      d.lat_lng_list
-        ? d.lat_lng_list.map((latlng) => LatLng.fromTuple(latlng))
+      d.latlng_list
+        ? d.latlng_list.map((latlng) => LatLng.fromTuple(latlng))
         : [],
     );
   }
