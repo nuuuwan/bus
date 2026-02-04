4dde20d90f50bba84225412f5429175e57365ce1
nuuuwan <nuuuwan@gmail.com>
2026-02-04 11:28:43 +0530

    Refactor HaltLink component to simplify layout and update display name property for improved clarity

--- src/view/moles/HaltLink.js
+++ src/view/moles/HaltLink.js
@@ -3,18 +3,8 @@ import PlaceIcon from "@mui/icons-material/Place";
 
 export default function HaltLink({ halt }) {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      gap={1}
-      sx={{
-        py: 1,
-        borderBottom: "1px solid",
-        borderColor: "divider",
-      }}
-    >
-      <PlaceIcon fontSize="small" />
-      <Typography variant="body1">{halt.name}</Typography>
+    <Box display="flex" alignItems="center" gap={1} sx={{}}>
+      <Typography variant="body1">{halt.displayName}</Typography>
     </Box>
   );
 }
