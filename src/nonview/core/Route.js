2a46bb71a394b54cf7fda33398f83d06bc962643
nuuuwan <nuuuwan@gmail.com>
2026-02-04 09:37:15 +0530

    Refactor Route class to streamline ID generation and enhance URL construction logic

--- src/nonview/core/Route.js
+++ src/nonview/core/Route.js
@@ -7,6 +7,15 @@ export default class Route {
     this.haltNameList = haltNameList;
     this.latLngList = latLngList;
   }
+
+  static getId(routeNum, direction) {
+    return `${routeNum}-${direction}`;
+  }
+
+  get id() {
+    return Route.getId(this.routeNum, this.direction);
+  }
+
   static fromPythonDict(d) {
     return new Route(
       d.route_num,
@@ -25,10 +34,11 @@ export default class Route {
     const routeSummaryDList = await WWW.fetchJSON(urlSummaryList);
 
     const routePromises = routeSummaryDList.map(async (dSummary) => {
+      const id = Route.getId(dSummary.route_num, dSummary.direction);
       const urlDetails =
         `https://raw.githubusercontent.com` +
         `/nuuuwan/bus_py/refs/heads/main/data/routes` +
-        `/${dSummary.route_num}-${dSummary.direction}.json`;
+        `/${id}.json`;
       const d = await WWW.fetchJSON(urlDetails);
       return Route.fromPythonDict(d);
     });
