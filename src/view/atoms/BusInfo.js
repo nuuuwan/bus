fdff7c358807b85186690107f7d480dff3c7e2b3
nuuuwan <nuuuwan@gmail.com>
2026-04-25 10:01:47 +0530

    Refactor BusInfo: simplify component by removing unused Box and RouteIconView

--- src/view/atoms/BusInfo.js
+++ src/view/atoms/BusInfo.js
@@ -1,12 +1,5 @@
-import { Box } from "@mui/material";
 import NumberPlate from "./NumberPlate";
-import RouteIconView from "./RouteIcon";
 
 export default function BusInfo({ bus }) {
-  return (
-    <Box display="flex" alignItems="center" gap={0.5} flexWrap="wrap">
-      <NumberPlate bus={bus} />
-      <RouteIconView route={bus.route} />
-    </Box>
-  );
+  return <NumberPlate bus={bus} />;
 }
